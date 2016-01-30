07287421e850d8cdbf554d70f6ed6e7aa6584203
Stian Grytøyr <stian@grytoyr.net>
2016-01-30 12:01:55 +0100

    Updated for modern React; Modified package.json

--- index.js
+++ index.js
@@ -4,106 +4,118 @@
  */
 'use strict';
 
-var React = require('react/addons');
-var dom = React.DOM;
+var React = require('react');
+var dom = require('react-dom');
+
+function cs(classes) {
+  var activeClasses = [];
+  Object.keys(classes).forEach(function(c) {
+    if (c) {
+      activeClasses.push(c);
+    }
+  });
+  return activeClasses.join(' ');
+}
 
 function createIcon(type) {
-    var iconType = type;
-    return React.createClass({
-        render: function () {
-            var type = iconType || this.props.type;
-            var cs = React.addons.classSet;
-            var classes = {
-                'fa': true,
-                'fa-spin': this.props.spin,
-                'fa-fw': this.props.fixedWidth,
-                'fa-li': this.props.li,
-                'fa-border': this.props.border
-            };
-            classes['fa-' + type] = type;
-            classes['fa-' + this.props.size] = this.props.size;
-            classes['fa-rotate-' + this.props.rotate] = this.props.rotate;
-            classes['fa-flip-' + this.props.flip] = this.props.flip;
-            classes['fa-stack-' + this.props.stack] = this.props.stack;
-            classes['fa-align-' + this.props.align] = this.props.align;
+  var iconType = type;
+  return React.createClass({
+    render: function () {
+      var type = iconType || this.props.type;
+      var classes = {
+        'fa': true,
+        'fa-spin': this.props.spin,
+        'fa-fw': this.props.fixedWidth,
+        'fa-li': this.props.li,
+        'fa-border': this.props.border
+      };
+      classes['fa-' + type] = type;
+      classes['fa-' + this.props.size] = this.props.size;
+      classes['fa-rotate-' + this.props.rotate] = this.props.rotate;
+      classes['fa-flip-' + this.props.flip] = this.props.flip;
+      classes['fa-stack-' + this.props.stack] = this.props.stack;
+      classes['fa-align-' + this.props.align] = this.props.align;
 
-            var className = cs(classes) + " " + (this.props.className || '');
+      var className = cs(classes) + " " + (this.props.className || '');
 
-            return this.transferPropsTo(
-                dom.i({className: className}, this.props.children)
-                );
-        }
-    });
+      return this.transferPropsTo(
+        dom.i({className: className}, this.props.children)
+        );
+    }
+  });
 }
 
 var IconStack = React.createClass({
-    render: function () {
-        var cs = React.addons.classSet;
-        var classes = {
-            'fa-stack': true
-        }
-        classes['fa-' + this.props.size] = this.props.size;
-
-        var className = cs(classes) + " " + (this.props.className || '');
-
-        return this.transferPropsTo(
-            dom.span({className: className}, this.props.children)
-            );
+  render: function () {
+    var classes = {
+      'fa-stack': true
     }
+    classes['fa-' + this.props.size] = this.props.size;
+
+    var className = cs(classes) + " " + (this.props.className || '');
+
+    return this.transferPropsTo(
+      dom.span({className: className}, this.props.children)
+      );
+  }
 });
 
 var Ul = React.createClass({
-    render: function () {
-        var cs = React.addons.classSet;
-        var classes = {
-            'fa-ul': true
-        }
-        var className = cs(classes) + " " + (this.props.className || '');
-
-        return this.transferPropsTo(
-            dom.ul({className: className}, this.props.children)
-            );
+  render: function () {
+    var classes = {
+      'fa-ul': true
     }
+    var className = cs(classes) + " " + (this.props.className || '');
+
+    return this.transferPropsTo(
+      dom.ul({className: className}, this.props.children)
+      );
+  }
 });
 
 var Icon = createIcon();
 
 var Animate = React.createClass({
-    getInitialState: function () {
-        return {
-            childCount: 0,
-            child: (dom.span({}, null))
-        };
-    },
-    componentWillMount: function () {
-        if (this.props.children) {
-            this.timer = setInterval(function () {
-                var newChild = this.state.childCount + 1;
-                if (this.props.children.length <= newChild) {
-                    newChild = 0;
-                }
-                this.setState({
-                    childCount: newChild,
-                    child: this.props.children[newChild]
-                });
-            }.bind(this), this.props.interval || 1000);
 
-            this.setState({
-                child: this.props.children[0]
-            });
+  getInitialState: function () {
+    return {
+      childCount: 0,
+      child: (dom.span({}, null))
+    };
+  },
+
+  componentWillMount: function () {
+    if (this.props.children) {
+      this.timer = setInterval(function () {
+        var newChild = this.state.childCount + 1;
+        if (this.props.children.length <= newChild) {
+          newChild = 0;
         }
-    },
-    componentWillUnmount: function () {
-        clearInterval(this.timer);
-    },
-    render: function () {
-        return this.state.child;
+        this.setState({
+          childCount: newChild,
+          child: this.props.children[newChild]
+        });
+      }.bind(this), this.props.interval || 1000);
+
+      this.setState({
+        child: this.props.children[0]
+      });
     }
+  },
+
+  componentWillUnmount: function () {
+    clearInterval(this.timer);
+  },
+
+  render: function () {
+    return this.state.child;
+  },
+
 });
 
 module.exports = {
-    Icon: Icon,
-    Ul: Ul,
-    IconStack: IconStack,
-    Animate: Animate
+  Icon: Icon,
+  Ul: Ul,
+  IconStack: IconStack,
+  Animate: Animate,
 };
